6600ead80334c17f08a8cd76e70492c1af4da77c
Surajit Maity <surajit.maity@capgemini.com>
2026-07-03 15:55:02 +0530

    feat: add initial Playwright test suite for homepage navigation and heading validation

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,22 +1,27 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
 
-  await test.step("Navigate to the Playwright homepage", async () => {
-     await page.goto('https://playwright.dev/');
+test.describe("Playwright test", () => {
+
+  test.beforeEach(async ({ page }) => {
+    await test.step("Navigate to the Playwright homepage", async () => {
+      await page.goto('https://playwright.dev/');
+    })
   })
 
-  // await test.step("Expect a title 'to contain' a substring.", async () => {
+
+  test('has title', async ({ page }) => {
     await expect(page).toHaveTitle(/Playwright/);
-  // })
-});
+  });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  test('get started link', async ({ page }) => {
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+    await test.step("Click the get started link", async () => {
+      await page.getByRole('link', { name: 'Get started' }).click();
+    })
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+    await test.step("Expects page to have a heading with the name of Installation", async () => {
+      await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+    })
+  });
 });
